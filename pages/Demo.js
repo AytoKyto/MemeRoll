d0b758b0dd85348bd160abd823101be1dfca7e92
Mathis <m.fleury942@gmail.com>
2021-02-10 16:28:20 +0100

    nav

--- pages/Demo.js
+++ pages/Demo.js
@@ -48,7 +48,7 @@ const styles = StyleSheet.create({
   },
   button: {
     alignItems: "center",
-    backgroundColor: "",
+    backgroundColor: "transparent",
     padding: 100,
   },
   countContainer: {
