d51250d6bd9b150603d18fa85ec312ae228db2e2
ertrasluapnaej <aeboissel@gmail.com>
2021-02-10 21:51:33 +0100

    oskour

--- components/SoundModule.js
+++ components/SoundModule.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, Image, View, TouchableOpacity } from 'react-native';
 import { Audio } from 'expo-av'
 
 const SoundModule = () => {
 
+
+    const sound = new Audio.Sound();
+    try {
+    sound.loadAsync(require('./assets/sounds/COUISINE.mp3'));
+      sound.playAsync();
+      // Your sound is playing!
+    
+      // Don't forget to unload the sound from memory
+      // when you are done using the Sound object
+     sound.unloadAsync();
+    } catch (error) {
+      // An error occurred!
+    }
+  
+  
+   useEffect(() => {
+      return sound 
+    }, [sound])
+
     return (
         <View style={styles.soundContainer}>
-            <TouchableOpacity style={styles.soundButton} >
+            <TouchableOpacity style={styles.soundButton} onPressIn={()=>playSound()}>
                 <Text>Prout</Text>
             </TouchableOpacity>
         </View>
     );
+    
 }
+
 
 const styles = StyleSheet.create({
     soundContainer:{
@@ -26,5 +47,6 @@ const styles = StyleSheet.create({
         borderRadius:20
     }
 })
+
 
 export default SoundModule;
